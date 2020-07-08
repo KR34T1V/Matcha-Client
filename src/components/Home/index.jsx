26db43d937541f9641eac69bad24bba3087da45b
MielieVeryStronk <nikel.emile@gmail.com>
2020-07-08 20:42:12 +0200

    added some more filters/search

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -7,9 +7,20 @@ import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import { NavLink } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+import Slider from '@material-ui/core/Slider';
+import Pagination from '@material-ui/lab/Pagination';
+import { Select, MenuItem } from '@material-ui/core';
 
 const Home = ({ classes, accessToken, setErrors, errors }) => {
+	const [unfilteredProfiles, setUnfilteredProfiles] = useState([]);
 	const [profiles, setProfiles] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('');
+	const [ageRange, setAgeRange] = useState([18, 100]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [maxPages, setMaxPages] = useState(1);
+	const [maxResults, setMaxResults] = useState(5);
+	const [sortValue, setSortValue] = useState('Username')
 
 	useEffect(() => {
 		const getProfiles = async () => {
@@ -24,16 +35,79 @@ const Home = ({ classes, accessToken, setErrors, errors }) => {
 			)
 				setErrors(errors);
 			if (data.data != null && data.data.length > 0) {
-				setProfiles(data.data);
+				setUnfilteredProfiles(data.data);
+				setProfiles(data.data.sort((a,b) => (a.Username > b.Username) ? 1 : ((b.Username > a.Username) ? -1 : 0)));
+				setMaxPages(data.data.length/maxResults);
 			}
 		};
 
 		getProfiles();
 	}, [accessToken, setErrors, errors]);
 
+	function updateProfiles() {
+		setProfiles(unfilteredProfiles.filter((data) => {
+			if((data.Firstname.toLowerCase().includes(searchTerm.toLowerCase()) ||
+				data.Lastname.toLowerCase().includes(searchTerm.toLowerCase()) || 
+				data.Username.toLowerCase().includes(searchTerm.toLowerCase())) &&
+				(data.Age >= ageRange[0] && data.Age <= ageRange[1])){
+				return data
+			} else {
+				return null
+			}
+		}))
+		setMaxPages(profiles.length/maxResults)
+	}
+
+	function handleChange(e) {
+		if (e.target.name === "searchInput") {
+			setSearchTerm(e.target.value)
+		}
+	}
+
 	return (
 		<>
-			{profiles.map(
+		<form className={classes.root} noValidate autoComplete="off">
+			<TextField
+				label="Search: "
+				type="input"
+				className={classes.tfield}
+				name="searchInput"
+				onChange={handleChange}
+			/>
+			<div className={classes.slider}>
+			<Typography id="range-slider" 
+					gutterBottom
+					color="secondary"
+					>
+				Age range
+			</Typography>
+			<Slider
+				name="ageRange"
+				value={ageRange}
+				onChange={(e, age) => setAgeRange(age)}
+				min={18}
+				step={1}
+				valueLabelDisplay="auto"
+				aria-labelledby="range-slider"
+				getArialValueText={ageRange}
+				/>
+			<Button
+			color="secondary"
+			variant="outlined"
+			onClick={updateProfiles}>
+				Filter
+				</Button>
+				<Select
+					value={sortValue}
+					className={classes.tfield}
+					onChange={(e) => setSortValue(e.target.value)}>
+					<MenuItem value={'Username'}>Username</MenuItem>
+					<MenuItem value={'Age'}>Age</MenuItem>
+				</Select>
+			</div>
+				</form>
+			{profiles.sort((a,b) => (a[sortValue] > b[sortValue]) ? 1 : ((b[sortValue] > a[sortValue]) ? -1 : 0))
+			.slice((currentPage-1)*maxResults, currentPage*maxResults).map(
 				({
 					Username,
 					Firstname,
@@ -139,6 +213,13 @@ const Home = ({ classes, accessToken, setErrors, errors }) => {
 					</Grid>
 				),
 			)}
+
+			<Pagination count={maxPages}
+			variant="outlined"
+			color="primary"
+			justify="center"
+			className={classes.pagenumber}
+			onChange={(e, page) => setCurrentPage(page)}/>
 		</>
 	);
 };
@@ -166,6 +247,43 @@ const styles = (theme) => ({
 		backgroundColor: theme.palette.primary.main,
 		marginTop: theme.spacing(1),
 		marginBottom: theme.spacing(1),
+	},
+	tfield: {
+		'& label.Mui-focused': {
+			color: theme.palette.secondary.main,
+		},
+		'& label': {
+			color: theme.palette.secondary.main,
+		},
+		'& .MuiInputBase-input': {
+			color: theme.palette.secondary.main,
+		},
+		'& .MuiInput-underline:before': {
+			borderBottomColor: theme.palette.secondary.main,
+		},
+		'& .MuiInput-underline:after': {
+			borderBottomColor: theme.palette.secondary.main,
+		},
+		'& .MuiInput-underline:hover:not(.Mui-disabled):before': {
+			borderBottomColor: theme.palette.secondary.main,
+		},
+		'& .MuiSelect-icon': {
+			color: theme.palette.secondary.main,
+		},
+	},
+	slider: {
+		width: 300,
+		'& .MuiSlider-thumb': {
+			color: theme.palette.secondary.main,
+		},
+		'& .MuiSlider-track': {
+			color: theme.palette.secondary.main,
+		},
+	},
+	pagenumber: {
+		'& .MuiPagination-ul': {
+			backgroundColor: theme.palette.secondary.main,
+		},
 	},
 });
 
